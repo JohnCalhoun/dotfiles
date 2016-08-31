200b706be32529bcb81ff33215f5d82efd9a2134
John M Calhoun <johnmcalhoun123@gmail.com>
2016-08-31 16:21:44 -0500

    hadoop aliases

---
+++ hidden/bashrc.d/alias.d/aws.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+alias als='aws s3 ls'
+alias acp='aws s3 cp'
+alias amv='aws s3 mv'
+alias arm='aws s3 rm'
+alias aweb='aws s3 website'
---
+++ hidden/bashrc.d/alias.d/hadoop.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+alias hls='hadoop fs -ls -h'
+alias hcat='hadoop fs -cat'
+alias hmkdir='hadoop fs -mkdir'
+alias hrm='hadoop fs -rm'
+alias hrmr='hadoop fs -rmdir'
+alias hdu='hadoop fs -du -s -h'
+alias hget='hadoop fs -get'
+alias hput='hadoop fs -put'
+alias hmv='hadoop fs -mv'
+alias hcp='hadoop fs -cp'
+alias hcount='hadoop fs -count'
